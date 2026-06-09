b66312b03a4fc14b3d1e5aff93389f7d1d6b8383
Sehar Andleeb <seharm518email@gmail.com>
2026-06-09 11:17:51 +0500

    Day 1: initialize repo, add .gitignore and first Python script

---
+++ day01/app.py
@@ -0,0 +1,9 @@
+
+# Author: Sehar Andleeb
+# Day 1 - AI Engineer Internship at Xeven Solutions
+
+print("Hello, Sehar!")
+print("Welcome to Xeven Solutions.")
+print("You are an AI Engineer Intern.")
+print("Today is Day 1 of your 30-day journey.")
+print("Let's get started!")
